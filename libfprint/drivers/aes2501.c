c615a71be0ef6c36df48d9461d2ceeada48ed9b9
Mark Harfouche <mark.harfouche@gmail.com>
2018-03-11 13:14:38 -0700

    Commented out unused static variables.
    As opposed to using __attribute__((unused))
    
    This fixed the following two warnings:
    ```
    drivers/aes1610.c:736:34: warning: ‘stop_reader’ defined but not used [-Wunused-const-variable=]
    drivers/aes2501.c:671:34: warning: ‘init_3’ defined but not used [-Wunused-const-variable=]
    ```
    
    The previous commit fixes -Wmisleading-indentation
    ```
    nbis/mindtct/morph.c:152:4: warning: this ‘if’ clause does not guard... [-Wmisleading-indentation]
    nbis/mindtct/morph.c:176:4: warning: this ‘if’ clause does not guard... [-Wmisleading-indentation]
    nbis/mindtct/morph.c:200:4: warning: this ‘if’ clause does not guard... [-Wmisleading-indentation]
    nbis/mindtct/morph.c:222:4: warning: this ‘if’ clause does not guard... [-Wmisleading-indentation]
    ```

--- libfprint/drivers/aes2501.c
+++ libfprint/drivers/aes2501.c
@@ -668,7 +668,8 @@ static const struct aes_regwrite init_2[] = {
 	{ AES2501_REG_CTRL1, AES2501_CTRL1_REG_UPDATE },
 };
 
-static const struct aes_regwrite init_3[] __attribute__((unused)) = {
+/*
+static const struct aes_regwrite init_3[] = {
 	{ 0xff, 0x00 },
 	{ AES2501_REG_CTRL1, AES2501_CTRL1_MASTER_RESET },
 	{ AES2501_REG_AUTOCALOFFSET, 0x41 },
@@ -676,6 +677,7 @@ static const struct aes_regwrite init_3[] __attribute__((unused)) = {
 	{ AES2501_REG_DETCTRL, 0x53 },
 	{ AES2501_REG_CTRL1, AES2501_CTRL1_REG_UPDATE },
 };
+*/
 
 static const struct aes_regwrite init_4[] = {
 	{ AES2501_REG_CTRL1, AES2501_CTRL1_MASTER_RESET },
